a527305d85ed1e34ccc0790af9e39fe9d8a82fd5
Eugenio <41469771+lugezz@users.noreply.github.com>
2022-06-08 06:22:47 -0300

    First Commit

---
+++ Clase 15/main.js
@@ -0,0 +1,14 @@
+let nombre = "Juan";
+
+nombre =
+
+    console.log(typeof nombre);
+
+const resultado = Math.sign(5);
+const resultado1 = Math.random() * 100;
+const resultado2 = Math.floor(5.2212);
+
+const resultado3 = prompt('Ingrese tu edad');
+
+console.log(resultado3);
+//aaklasklsakl
